5c83e1f6bae641670f9f3d0167b8c95b391ad829
Linus Gasser <ineiti@linusetviviane.ch>
2014-11-11 21:59:54 +0100

    lots of work in the Network-module

--- lib/helperclasses/System.rb
+++ lib/helperclasses/System.rb
@@ -2,16 +2,20 @@
 
 module System
   extend self
+  extend HelperClasses::DPuts
 
   def run_str( cmd )
+    dputs(3){"Running command #{cmd}"}
     %x[ #{cmd} ]
   end
 
   def run_bool( cmd )
-    Kernel.system( cmd )
+    dputs(3){"Running command #{cmd}"}
+    Kernel.system( "#{cmd} > /dev/null 2>&1")
   end
 
   def exists?( cmd )
+    dputs(3){"Exist command #{cmd}?"}
     run_bool( "which #{cmd} > /dev/null 2>&1")
   end
 end
