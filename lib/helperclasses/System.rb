89dc9a652906e703ffb5ae937a4cfdcf615a4363
Linus Gasser <ineiti@linusetviviane.ch>
2014-11-25 22:48:32 +0100

    for the nth time, a working airtel implementation

--- lib/helperclasses/System.rb
+++ lib/helperclasses/System.rb
@@ -8,17 +8,17 @@ module HelperClasses
     include HelperClasses::DPuts
 
     def run_str(cmd)
-      dputs(3) { "Running command #{cmd}" }
+      dputs(3) { "Running command --#{cmd}--" }
       %x[ #{cmd} ]
     end
 
     def run_bool(cmd)
-      dputs(3) { "Running command #{cmd}" }
+      dputs(3) { "Running command --#{cmd}--" }
       Kernel.system("#{cmd} > /dev/null 2>&1")
     end
 
     def exists?(cmd)
-      dputs(3) { "Exist command #{cmd}?" }
+      dputs(3) { "Exist command --#{cmd}--?" }
       run_bool("which #{cmd} > /dev/null 2>&1")
     end
 
