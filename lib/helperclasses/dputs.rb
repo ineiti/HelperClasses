cffda447308c89f3d4bdec5a43cedaab903d1e5c
Linus Gasser <ineiti@linusetviviane.ch>
2015-04-25 01:14:48 +0100

    cleaned up configbase and config.yaml

--- lib/helperclasses/dputs.rb
+++ lib/helperclasses/dputs.rb
@@ -26,24 +26,26 @@ module HelperClasses
           when /hour/
             show = (now.to_i / 3600).floor != ($dputs_time.to_i / 3600).floor
         end
-        show and puts "\n   *** It is now: " +
-                          Time.now.strftime("%Y-%m-%d %H:%M:%S")
-        $dputs_time = now
+        if show
+          puts "\n   *** It is now: " +
+                   Time.now.strftime('%Y-%m-%d %H:%M:%S')
+          $dputs_time = now
+        end
       end
       DPuts.mutex.synchronize do
-        width = DPuts.terminal_width
-        width -= 30.0
-        file, func = call.split(" ")
+        width = DPuts.terminal_width.to_i || 160
+        width = [width - 30.0, 10].max
+        file, func = call.split(' ')
         file = file[/^.*\/([^.]*)/, 1]
-        who = (":" + n.to_s + ":" + file.to_s +
-            func.to_s).ljust(30, ["X", "x", "*", "-", ".", " "][n])
+        who = (':' + n.to_s + ':' + file.to_s +
+            func.to_s).ljust(30, ['X', 'x', '*', '-', '.', ' '][n])
         lines = []
         pos = 0
         while (pos < s.length)
           len = width
           if s.length - pos > width
             len = s.rindex(/[, .;=&>]/, pos + width)
-            len and len = len - pos + 1
+            len = len ? len - pos + 1 : width
             if len < width / 2
               len = width
             end
@@ -51,9 +53,9 @@ module HelperClasses
           lines.push s.slice(pos, len)
           pos += len
         end
-        puts who + " " + lines.shift.to_s
+        puts who + ' ' + lines.shift.to_s
         lines.each { |l|
-          puts " " * (32) + l
+          puts ' ' * (32) + l
         }
       end
     end
@@ -75,7 +77,7 @@ module HelperClasses
 
     def dputs(n, &s)
       n *= -1 if ($DPUTS_FUNCS and $DPUTS_FUNCS.index(dputs_getcaller))
-      if  !self.class.const_defined?(:DEBUG_LVL) or
+      if !self.class.const_defined?(:DEBUG_LVL) or
           self.class.const_get(:DEBUG_LVL) >= n
         s = yield s
         dputs_out(n, s, caller(0)[1])
